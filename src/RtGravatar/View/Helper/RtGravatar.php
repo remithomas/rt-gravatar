d377f169ecedd6b61ba3c8478d56aa4711bf78dd
Rémi THOMAS <remi.thomas@yahoo.fr>
2013-03-29 14:48:02 +0100

    Translation

--- src/RtGravatar/View/Helper/RtGravatar.php
+++ src/RtGravatar/View/Helper/RtGravatar.php
@@ -26,9 +26,9 @@ class RtGravatar extends AbstractHelper
         
         // edit
         if($edit && $connected){
-            
+            $changeMe = $this->view->translate("change me");
             $size = (isset($options['img_size']) ? $options['img_size'] : 80);
-            $editAction = "<span id='editgravatar' style='background:#FFF;color:#000;position:absolute;display:none;font-size:".round($size/8)."px;text-align:center;width:".$size."px;text-decoration:none;padding:4px 0px;-webkit-transition-duration: 0.21799999475479126s;-webkit-transition-property: background-color;-webkit-transition-timing-function: cubic-bezier(0.42, 0, 0.58, 1);opacity:0.6;filter:alpha(opacity=60);filter:progid:DXImageTransform.Microsoft.Alpha(opacity=60);-ms-filter:\"alpha(opacity=60)\";-moz-opacity:0.6;-khtml-opacity:0.6;'>change me</span>";
+            $editAction = "<span id='editgravatar' style='background:#FFF;color:#000;position:absolute;display:none;font-size:".round($size/8)."px;text-align:center;width:".$size."px;text-decoration:none;padding:4px 0px;-webkit-transition-duration: 0.21799999475479126s;-webkit-transition-property: background-color;-webkit-transition-timing-function: cubic-bezier(0.42, 0, 0.58, 1);opacity:0.6;filter:alpha(opacity=60);filter:progid:DXImageTransform.Microsoft.Alpha(opacity=60);-ms-filter:\"alpha(opacity=60)\";-moz-opacity:0.6;-khtml-opacity:0.6;'>".$changeMe."</span>";
             return "<a href='http://gravatar.com' onmouseout='javascript:document.getElementById(\"editgravatar\").style.display=\"none\";' onmouseover='javascript:document.getElementById(\"editgravatar\").style.display=\"block\";'>".$editAction."".$gravatar."</a>";
         }else{
             return $gravatar;
